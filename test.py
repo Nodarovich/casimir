5b81e4e18f7730e3f62c9d201492e278804487a0
Nodarovich <mr.doda@gmail.com>
2017-11-09 13:16:14 +0100

    second commit

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("hello world")
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+def surface(r):
+    """This function calculates the surface of a circle"""
+    area = np.pi*r**2
+    return area
+
+def circumference(r):
+    """This function calculates the circumference"""
+    return 2*np.pi*r
+
+def f(x):
+    return np.sin(x)
+
+def plot(a,b,c):
+    x= np.arange(a,b,c)
+    plt.plot(x, f(x))
+    plt.show()
+    print(np.size(x))
